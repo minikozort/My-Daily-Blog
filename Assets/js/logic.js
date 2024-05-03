212b4879ace5a5ee21f5535c2a0aa40a33ddd76c
minikozort <gokselgokkocabas@gmail.com>
2024-05-03 16:53:12 -0400

    edited css

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,9 +1,7 @@
 const body = document.querySelector('body');
-const nav = document.querySelector('nav');
 const toggle = document.getElementById('togglemode');
 toggle.onclick = function () {
     toggle.classList.toggle('active');
-    nav.classList.toggle('active');
     body.classList.toggle('active');
     
 }
