301aa32b9bf9e1053827cce5d83b4d4f090631fb
minikozort <gokselgokkocabas@gmail.com>
2024-05-03 16:00:15 -0400

    fixed the toggle switch code

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,6 +1,9 @@
-const nav = document.querySelector('nav')
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
 const toggle = document.getElementById('togglemode');
 toggle.onclick = function () {
-    toggle.classList.toggle('active')
-    nav.classList.toggle('active')
+    toggle.classList.toggle('active');
+    nav.classList.toggle('active');
+    body.classList.toggle('active');
+    
 }
