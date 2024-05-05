28be2121b751cbfd58178b25dd2a51c58cd1fcc1
minikozort <gokselgokkocabas@gmail.com>
2024-05-05 11:54:41 -0400

    edited all files

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,7 +1,10 @@
 const body = document.querySelector('body');
 const toggle = document.getElementById('togglemode');
+const form = document.querySelector('form');
 toggle.onclick = function () {
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+    form.classList.toggle('active');
+    
     
 }
