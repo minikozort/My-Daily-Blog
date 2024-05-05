88c241ba698c25e4e84ad985f2d740da548276dd
minikozort <gokselgokkocabas@gmail.com>
2024-05-05 13:10:39 -0400

    edited codes

---
+++ Assets/js/blog.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+const toggle = document.getElementById('togglemode');
+toggle.onclick = function () {
+    toggle.classList.toggle('active');
+    body.classList.toggle('active')
+    
+    
+}
