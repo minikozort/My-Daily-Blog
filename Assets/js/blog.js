eaee7712c65d44b86a70ba37e0e9944fd59f83eb
minikozort <gokselgokkocabas@gmail.com>
2024-05-07 22:39:46 -0400

    added a for loop for element creation

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -8,28 +8,36 @@ toggle.onclick = function () {
     
 }
 
-
 function getPost() {
     // Use JSON.parse() to convert text to JavaScript object
-    var blogItem = JSON.parse(localStorage.getItem('userInput'));
+    const blogItem = JSON.parse(localStorage.getItem('userInput'));
 
     console.log(blogItem);
     // Check if data is returned, if not exit out of the function
     if (blogItem !== null) {
-    
+    for (i = 0; i < blogItem.length; ++i) {
+        let article = document.createElement('article');
+        article.setAttribute('class', 'article');
+        article.id = 'article'+i;
+        document.body.appendChild(article);
 
-    //   document.getElementById('user_first_name') = blogItem.firstName;
-    //   document.getElementById('user_last_name') = blogItem.lastName;
-    //   document.getElementById('userblog') = blogItem.userBlogEntry;
+        //for (var key in blogItem[i]) {
+        blogItem.forEach((element) => {
+            let card = document.createElement('card');
+            card.id = 'card'+i;
+            card.setAttribute('class', 'card');
+            card.innerHTML = element.firstName;
+            document.getElementById('article'+i).appendChild(card);
+            console.log(blogItem[i])
+        })
 
     }
 
-
-  }
+    //for ()
+  }}
 
   function init() {
     // When the init function is executed, the code inside renderLastGrade function will also execute
     getPost();
   }
   init();
-  
